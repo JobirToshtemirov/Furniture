d609e0abf5be24300969eb38130819b047aad667
SanjarbekAI <sanjarbekwork@gmail.com>
2024-10-16 16:07:12 +0500

    changed

--- pages/views.py
+++ pages/views.py
@@ -4,8 +4,6 @@ from django.views.generic import TemplateView
 
 
 class ContactView(TemplateView):
-    text = _("Your form acadcda wcw")
-    messages.success(text)
     template_name = 'contact.html'
 
 
